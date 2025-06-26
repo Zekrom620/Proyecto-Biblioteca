002429526c3858a62b116746b92b2c3a05df37ba
Zekrom620 <ser.aranguiz@duocuc.cl>
2025-06-25 21:11:11 -0400

    Primer versionamiento biblio

---
+++ src/main/java/com/biblioteca/cl/pruebaBiblioteca/PruebaBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.cl.pruebaBiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaBibliotecaApplication.class, args);
+	}
+
+}
